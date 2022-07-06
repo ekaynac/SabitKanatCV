087a212e8aa62f341a5dc26ef6e9014297769b49
Enes KAYNAKCI <enes_kaynakci@hotmail.com>
2022-07-06 19:19:16 +0300

    06.07.2022

--- RedDetection/detector.py
+++ RedDetection/detector.py
@@ -1,5 +1,5 @@
 import cv2 as cv
-import time
+
 class cvDetector:
     # Empty function for slider arguments
     @staticmethod
--- RedDetection/reddetection.py
+++ RedDetection/reddetection.py
@@ -13,7 +13,7 @@ cv.createTrackbar("U_S","Filter",255,255,nothing)
 cv.createTrackbar("L_V","Filter",0,255,nothing)
 cv.createTrackbar("U_V","Filter",255,255,nothing)
 
-cap = cv.VideoCapture(r"D:\GitHub\SabitKanatCV\RedDetection\bluedetection.mp4")
+cap = cv.VideoCapture(r"D:\GitHub\SabitKanatCV\RedDetection\reddetection.mp4")
 
 kernel_dilation = np.ones(5, np.uint8)
 kernel_erosion = np.ones(5, np.uint8)
@@ -23,7 +23,7 @@ while(True):
     if ret:
         frame_hsv = cv.cvtColor(frame,cv.COLOR_BGR2HSV)
 
-        frame_hsv_blured = cv.GaussianBlur(frame_hsv,(5,5),0)
+        frame_hsv_blured = cv.GaussianBlur(frame_hsv,(5,5),7)
         
         l_h = cv.getTrackbarPos("L_H","Filter")
         u_h = cv.getTrackbarPos("U_H","Filter")
@@ -38,10 +38,10 @@ while(True):
         u_bound = np.array([u_h,u_s,u_v])
         
         mask = cv.inRange(frame_hsv,l_bound,u_bound)
-        mask_refined = cv.erode(mask, kernel_erosion, iterations=5)
+        mask_refined = cv.erode(mask, kernel_erosion, iterations=10)
         mask_refined = cv.dilate(mask_refined, kernel_dilation, iterations=5)
         contours, _ = cv.findContours(mask_refined, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-        frame, x_list, y_list = cvDetector.drawRectangle(frame, contours, area_thresh= (0, 50000), rectangle_color=(255,0,0), rectangle_thickness=2)
+        frame, x_list, y_list = cvDetector.drawRectangle(frame, contours, area_thresh= (1000, 50000), rectangle_color=(255,0,0), rectangle_thickness=2)
         frame = cvDetector.distfromcenter(frame, x_list, y_list)
         cv.imshow("frame",frame)
         cv.imshow("frame_hsv",frame_hsv)
